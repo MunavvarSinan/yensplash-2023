9ba29e4cc3e9e29e799203c7c4a460a04dd406c8
MunavvarSinan <munavvarsinan8@gmail.com>
2023-05-16 23:15:39 +0530

    minor changes

--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -48,7 +48,7 @@ const EventDetails = () => {
                                 priority
                             />
                             <div className="mt-6 flex justify-between ">
-                                <p className="flex items-center text-gray-500">
+                                <p className="flex items-center  text-gray-500">
                                     <svg style={{ color: 'white', marginRight: 10 }} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-calendar-day" viewBox="0 0 16 16"> <path d="M4.684 11.523v-2.3h2.261v-.61H4.684V6.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V8.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105zm2.805-5.093c0 .238.192.425.43.425a.428.428 0 1 0 0-.855.426.426 0 0 0-.43.43zm.094 5.093h.672V7.418h-.672v4.105z" fill="white"></path> <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" fill="white"></path> </svg>
                                     {event.schedule.day}
                                 </p>
@@ -73,14 +73,14 @@ const EventDetails = () => {
                         <div className="md:p-10 ">
                             <h4 className='text-3xl  font-bold font-headings md:text-5xl'>
                                 About Event.
-                            </h4>                        <p className='md:p-5 mt-4'>{event.aboutEvent}</p>
+                            </h4>                        <p className='md:p-5 text-justify mt-4'>{event.aboutEvent}</p>
                         </div>
                         <div className="md:p-10">
                             <h4 className='text-3xl font-bold  font-headings md:text-5xl'>
                                 Event Details.
                             </h4>                          <ul className="list-disc pl-4 mt-4">
                                 {event.eventDetail.map((item, index) => (
-                                    <li className='lg:p-2 lg:ml-10 ' key={index}>{item}</li>
+                                    <li className='lg:p-2 text-justify lg:ml-10 ' key={index}>{item}</li>
                                 ))}
                             </ul>
                         </div>
@@ -89,7 +89,7 @@ const EventDetails = () => {
                                 Rules and Regulations</h4>
                             <ul className="list-disc lg:pl-4 mt-5 ">
                                 {event.rules.map((item, index) => (
-                                    <li className='lg:p-2 lg:ml-10' key={index}>{item}</li>
+                                    <li className='lg:p-2 text-justify lg:ml-10' key={index}>{item}</li>
                                 ))}
                             </ul>
                         </div>
@@ -98,7 +98,7 @@ const EventDetails = () => {
                                 Judging Criteria</h4>
                             <ul className="list-disc pl-4 mt-5">
                                 {event.judgingCriteria.map((item, index) => (
-                                    <li className='lg:p-2 lg:ml-10' key={index}>{item}</li>
+                                    <li className='lg:p-2 text-justify lg:ml-10' key={index}>{item}</li>
                                 ))}
                             </ul>
                         </div>
@@ -107,7 +107,7 @@ const EventDetails = () => {
                                 Student Coordinators</h4>
                             <div className="space-y-2 mt-5">
                                 {event.coordinators.students.map((item, index) => (
-                                    <p className='lg:p-2 lg:ml-10 ' key={index}>
+                                    <p className='lg:p-2 lg:ml-10 text-justify ' key={index}>
                                         {item.name} (<a href={`tel:${item.phone}`}>{item.phone}</a>)
                                     </p>
                                 ))}
@@ -118,7 +118,7 @@ const EventDetails = () => {
                                 Faculty Coordinators</h4>
                             <div className="space-y-2 mt-5 ">
                                 {event.coordinators.faculty.map((item, index) => (
-                                    <p className='lg:p-2 lg:ml-10 ' key={index}>Prof. {item}</p>
+                                    <p className='lg:p-2 text-justify lg:ml-10 ' key={index}>Prof. {item}</p>
                                 ))}
                             </div>
                         </div>
