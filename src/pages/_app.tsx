be06cc4d641e19c048f75dc5ac3453161b76e1b8
MunavvarSinan <munavvarsinan8@gmail.com>
2023-05-18 12:58:36 +0530

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import { store } from '@/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Provider store={store}>  <Component {...pageProps} /></Provider>
+  return <Provider store={store}>  <Component {...pageProps} /> <Analytics /></Provider>
 }
